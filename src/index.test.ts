d8ef4c91dceaf3bcd6346b8df50b54467ae1149d
airtoxin <airtoxin@icloud.com>
2021-01-09 06:51:58 +0900

    Add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,21 @@ import { CNFBuilder, CNFClause } from "./index";
 import dedent from "ts-dedent";
 
 describe("Builder", () => {
-  describe("AND clause", () => {
-    it("should build CNF format string from single clause", () => {
-      const builder = new CNFBuilder();
+  const createDefaultContext = () => {
+    const builder = new CNFBuilder();
 
-      const A = builder.addVariable("A");
-      const B = builder.addVariable("B");
-      const C = builder.addVariable("C");
+    const A = builder.addVariable("A");
+    const B = builder.addVariable("B");
+    const C = builder.addVariable("C");
+    const D = builder.addVariable("D");
+    const E = builder.addVariable("E");
+
+    return { builder, A, B, C, D, E };
+  };
+
+  describe("and clause", () => {
+    it("should build CNF format string from single clause", () => {
+      const { builder, A, B, C } = createDefaultContext();
 
       builder.addClause(CNFClause.and([A, B.not, C]));
 
@@ -20,31 +28,25 @@ describe("Builder", () => {
     });
 
     it("should build CNF format string from multiple clauses", () => {
-      const builder = new CNFBuilder();
+      const { builder, A, B, C, D, E } = createDefaultContext();
 
-      const A = builder.addVariable("A");
-      const B = builder.addVariable("B");
-      const C = builder.addVariable("C");
-
-      builder.addClause(CNFClause.and([A, B.not]));
-      builder.addClause(CNFClause.and([B.not, C]));
+      builder.addClause(CNFClause.and([A, B.not, C]));
+      builder.addClause(CNFClause.and([C, D, E.not]));
 
       expect(builder.build()).toEqual(dedent`
         1 0
         -2 0
-        -2 0
         3 0
+        3 0
+        4 0
+        -5 0
       `);
     });
   });
 
-  describe("OR clause", () => {
+  describe("or clause", () => {
     it("should build CNF format string from single clause", () => {
-      const builder = new CNFBuilder();
-
-      const A = builder.addVariable("A");
-      const B = builder.addVariable("B");
-      const C = builder.addVariable("C");
+      const { builder, A, B, C } = createDefaultContext();
 
       builder.addClause(CNFClause.or([A, B.not, C]));
 
@@ -54,18 +56,101 @@ describe("Builder", () => {
     });
 
     it("should build CNF format string from multiple clauses", () => {
-      const builder = new CNFBuilder();
+      const { builder, A, B, C, D, E } = createDefaultContext();
 
-      const A = builder.addVariable("A");
-      const B = builder.addVariable("B");
-      const C = builder.addVariable("C");
-
-      builder.addClause(CNFClause.or([A, B.not]));
-      builder.addClause(CNFClause.or([B.not, C]));
+      builder.addClause(CNFClause.or([A, B.not, C]));
+      builder.addClause(CNFClause.or([C, D, E.not]));
 
       expect(builder.build()).toEqual(dedent`
-        1 -2 0
-        -2 3 0
+        1 -2 3 0
+        3 4 -5 0
+      `);
+    });
+  });
+
+  describe("atMostOne clause", () => {
+    it("should build CNF format string from single clause", () => {
+      const { builder, A, B, C } = createDefaultContext();
+
+      builder.addClause(CNFClause.atMostOne([A, B.not, C]));
+
+      expect(builder.build()).toEqual(dedent`
+        -1 2 0
+        -1 -3 0
+        2 -3 0
+      `);
+    });
+
+    it("should build CNF format string from multiple clauses", () => {
+      const { builder, A, B, C, D, E } = createDefaultContext();
+
+      builder.addClause(CNFClause.atMostOne([A, B.not, C]));
+      builder.addClause(CNFClause.atMostOne([C, D, E.not]));
+
+      expect(builder.build()).toEqual(dedent`
+        -1 2 0
+        -1 -3 0
+        2 -3 0
+        -3 -4 0
+        -3 5 0
+        -4 5 0
+      `);
+    });
+  });
+
+  describe("atLeastOne clause", () => {
+    it("should build CNF format string from single clause", () => {
+      const { builder, A, B, C } = createDefaultContext();
+
+      builder.addClause(CNFClause.atLeastOne([A, B.not, C]));
+
+      expect(builder.build()).toEqual(dedent`
+        1 -2 3 0
+      `);
+    });
+
+    it("should build CNF format string from multiple clauses", () => {
+      const { builder, A, B, C, D, E } = createDefaultContext();
+
+      builder.addClause(CNFClause.atLeastOne([A, B.not, C]));
+      builder.addClause(CNFClause.atLeastOne([C, D, E.not]));
+
+      expect(builder.build()).toEqual(dedent`
+        1 -2 3 0
+        3 4 -5 0
+      `);
+    });
+  });
+
+  describe("exactlyOne clause", () => {
+    it("should build CNF format string from single clause", () => {
+      const { builder, A, B, C } = createDefaultContext();
+
+      builder.addClause(CNFClause.exactlyOne([A, B.not, C]));
+
+      expect(builder.build()).toEqual(dedent`
+        -1 2 0
+        -1 -3 0
+        2 -3 0
+        1 -2 3 0
+      `);
+    });
+
+    it("should build CNF format string from multiple clauses", () => {
+      const { builder, A, B, C, D, E } = createDefaultContext();
+
+      builder.addClause(CNFClause.exactlyOne([A, B.not, C]));
+      builder.addClause(CNFClause.exactlyOne([C, D, E.not]));
+
+      expect(builder.build()).toEqual(dedent`
+        -1 2 0
+        -1 -3 0
+        2 -3 0
+        1 -2 3 0
+        -3 -4 0
+        -3 5 0
+        -4 5 0
+        3 4 -5 0
       `);
     });
   });
