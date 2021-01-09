64360e755840db0c6f5646dccb138e8b67a4dce7
airtoxin <airtoxin@icloud.com>
2021-01-09 10:24:13 +0900

    Add alwaysTrue, alwaysFalse

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { combination } from "./utils";
 class CNFVariable {
   constructor(
     public readonly name: string,
-    public readonly num: number,
+    public readonly sequence: number,
     public readonly isNot: boolean = false,
     public readonly id: Symbol = Symbol(name)
   ) {}
 
   get not(): CNFVariable {
-    return new CNFVariable(this.name, this.num, !this.isNot, this.id);
+    return new CNFVariable(this.name, this.sequence, !this.isNot, this.id);
   }
 }
+
+const AlwaysTrueVariable = new CNFVariable("ALWAYS_TRUE", 1);
+const AlwaysFalseVariable = new CNFVariable("ALWAYS_FALSE", 2);
 
 export type Clause = CNFVariable[];
 
@@ -19,6 +22,14 @@ export class CNFClause {
   public readonly variables: Set<Symbol>;
   constructor(public readonly clauses: Clause[]) {
     this.variables = new Set(clauses.flat().map((v) => v.id));
+  }
+
+  static alwaysTrue(): CNFClause {
+    return new CNFClause([[AlwaysTrueVariable]]);
+  }
+
+  static alwaysFalse(): CNFClause {
+    return new CNFClause([[AlwaysFalseVariable]]);
   }
 
   static and(vars: CNFVariable[]): CNFClause {
@@ -56,7 +67,8 @@ export type CNFBuilderOptions = {
 };
 
 export class CNFBuilder {
-  private variableCount = 0;
+  // sequence=1 and sequence=2 used to AlwaysTrueVariable, AlwaysFalseVariable
+  private nextVariableSeq = 3;
   private variables: Set<Symbol> = new Set();
   private clauses: Clause[] = [];
   private comments: string[] = [];
@@ -82,8 +94,7 @@ export class CNFBuilder {
   }
 
   public addVariable(name: string): CNFVariable {
-    this.variableCount += 1;
-    return new CNFVariable(name, this.variableCount);
+    return new CNFVariable(name, this.nextVariableSeq++);
   }
 
   public addClause(clause: CNFClause): void {
@@ -94,9 +105,21 @@ export class CNFBuilder {
   public build(): string {
     const commentLines = this.comments.map((c) => `c ${c}`);
     const headerLines = [`p cnf ${this.variables.size} ${this.clauses.length}`];
+
+    // Reassign the sequence to remove sequence that assigned to the unused variable.
+    let newSequence = 1;
+    const variableNumberMap = new Map<Symbol, number>();
+
     const clauseLines = this.clauses.map((clause) =>
       clause
-        .map((v) => (v.isNot ? `-${v.num}` : `${v.num}`))
+        .map((variable) => {
+          const isNot = variable.isNot;
+          const sequence =
+            variableNumberMap.get(variable.id) ||
+            variableNumberMap.set(variable.id, newSequence++).get(variable.id)!;
+
+          return isNot ? `-${sequence}` : `${sequence}`;
+        })
         .concat(["0"])
         .join(" ")
     );
